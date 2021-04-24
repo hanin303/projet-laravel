11de819226e97f78ff39e7b0e87abf88edc04b80
NourG8 <nourguerfali08@gmail.com>
2021-04-24 02:17:21 +0100

    commit1

---
+++ database/migrations/2021_04_24_004108_create_paniers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaniersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paniers', function (Blueprint $table) {
+            $table->increments('id_panier');
+            //$table->integer('id_produit')->unsigned();
+            $table->integer('id_commande');
+            $table->integer('Quantite');
+            $table->timestamps();
+           //$table->foreign('id_produit')->reference('id_produit')->on('produits')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paniers');
+    }
+}
