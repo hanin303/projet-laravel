7510965439c1e7be486647a5c420f934e31822c9
NourG8 <nourguerfali08@gmail.com>
2021-04-29 12:18:36 +0100

    commit

--- app/Panier.php
+++ app/Panier.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Panier extends Model
 {
-    //
+    public function produits(){
+        return $this->hasMany('App\Produit');
+    }
+    
+    public function cammande(){
+        return $this->belongsTo('App/Cammnade');
+    }
 }
