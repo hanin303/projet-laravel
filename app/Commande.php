341f08e107b8feedf363047d1a51b330eecdefd9
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-04-30 01:58:36 +0200

    maryam_migrations

---
+++ app/Commande.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Commande extends Model
+{
+  public function client()
+  {
+      return $this->belongsTo('App\Client');
+  }
+
+  /*
+  public function details()
+  {
+      return $this->hasMany('App\Detail');
+  }*/
+}
