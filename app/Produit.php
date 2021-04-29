4373767086df24ee70c1395d74bbc325ab127c35
NourG8 <nourguerfali08@gmail.com>
2021-04-30 00:58:25 +0100

    nour_migrations

---
+++ app/Produit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produit extends Model
+{
+    public function details()
+    {
+        return $this->hasMany('App\Detail');
+    }
+  /*  public function commande()
+    {
+        return $this->belongsTo('App\Commande');
+    }*/
+}
---
+++ database/migrations/2021_04_29_234313_create_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('produit_id')->unsigned();
+    //      $table->integer('commande_id')->unsigned();
+            $table->integer('Quantite');
+            $table->timestamps();
+            $table->foreign('produit_id')->references('id')->on('produits')->onDelete('restrict')->onUpdate('restrict');
+           // $table->foreign('commande_id')->references('id')->on('commandes')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+}
