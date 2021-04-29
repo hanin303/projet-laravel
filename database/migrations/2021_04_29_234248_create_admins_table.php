97b201d41c7979431624824c0d1cce7574467a7a
hanin303 <benjemaahanin@gmail.com>
2021-04-30 00:58:25 +0100

    hanin_migration

---
+++ database/migrations/2021_04_29_234248_create_admins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('password');
+            $table->integer('type_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
