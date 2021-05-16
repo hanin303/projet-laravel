9cd036e9710c377e87867203d03951b45c742ae2
hanin303 <benjemaahanin@gmail.com>
2021-05-16 21:56:41 +0100

    HANIN_ADMIN_CRUD1

---
+++ resources/views/Admin/index.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 70%;
+    }
+    
+    th, td {
+      text-align: left;
+      padding: 8px;
+    }
+    
+    tr:nth-child(even) {background-color: #f2f2f2;}
+    </style>
+</head>
+<body>
+
+{{----------------------------------- standard navigation --------------------------- --}}
+@extends('layouts.dashboard')
+@section('content')
+
+<div class="pcoded-main-container">
+                <div class="pcoded-wrapper">
+            
+             <h3>Admins List</h3>
+             <div>
+             <table>
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">phone</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($admins as $Key => $admin)
+                        
+                  <tr>
+                    <th scope="row">{{ $Key }}</th>
+                    <td>{{$admin ->first_Name}}</td>
+                    <td>{{$admin ->email_admin}}</td>
+                    <td>{{$admin ->phone_admin}}</td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+             </div>
+
+
+
+
+                    
+                </div>
+                <div class="fixed-button">
+                    <a href="https://codedthemes.com/item/guru-able-admin-template/" target="_blank" class="btn btn-md btn-primary">
+                      <i class="fa fa-shopping-cart" aria-hidden="true"></i> Upgrade To Pro
+                    </a>
+                </div>
+            </div>
+        </div>
+
+<!-- Required Jquery -->
+<script type="text/javascript" src="assets/js/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.min.js"></script>
+<script type="text/javascript" src="assets/js/popper.js/popper.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap/js/bootstrap.min.js"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="assets/js/jquery/jquery.min.js"></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="assets/js/modernizr/modernizr.js"></script>
+<!-- am chart -->
+<script src="assets/pages/widget/amchart/amcharts.min.js"></script>
+<script src="assets/pages/widget/amchart/serial.min.js"></script>
+<!-- Todo js -->
+<script type="text/javascript " src="assets/pages/todo/todo.js "></script>
+<!-- Custom js -->
+<script type="text/javascript" src="assets/pages/dashboard/custom-dashboard.js"></script>
+<script type="text/javascript" src="assets/js/script.js"></script>
+<script type="text/javascript " src="assets/js/SmoothScroll.js"></script>
+<script src="assets/js/pcoded.min.js"></script>
+<script src="assets/js/demo-12.js"></script>
+<script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script>
+var $window = $(window);
+var nav = $('.fixed-button');
+    $window.scroll(function(){
+        if ($window.scrollTop() >= 200) {
+         nav.addClass('active');
+     }
+     else {
+         nav.removeClass('active');
+     }
+ });
+</script>
+@endsection
+  
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'HomeController@dashboardAdmin')->middleware('auth','admin');
+Route::resource('admins' , 'Admin\AdminController');
 
